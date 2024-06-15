4c573d0dbc206aade5bac76dc3fa222599bc3c94
Bombollini <rizhanmunra@gmail.com>
2024-06-15 22:17:17 +0700

    database pakai yg ini biar sync

---
+++ database/db_sig4.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 15, 2024 at 05:16 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_sig4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ibadah`
+--
+
+CREATE TABLE `ibadah` (
+  `kd_tempat` varchar(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `jenis` varchar(20) NOT NULL,
+  `alamat` varchar(30) NOT NULL,
+  `kd_kelurahan` varchar(11) NOT NULL,
+  `kapasitas` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelurahan`
+--
+
+CREATE TABLE `kelurahan` (
+  `kd_kelurahan` varchar(11) NOT NULL,
+  `nama_kelurahan` varchar(25) NOT NULL,
+  `jumlah_tempat_ibadah` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(25) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `level` enum('administrator','pengguna') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`username`, `password`, `level`) VALUES
+('1212121', 'qwer', 'administrator'),
+('2210010295', 'rizhanganteng123', 'administrator'),
+('22100106777', 'rizhan123', 'pengguna'),
+('22100106969', 'rizhan123', 'administrator'),
+('2210016666', 'nurganteng123', 'administrator'),
+('69696969', 'asdasdasd', 'administrator');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ibadah`
+--
+ALTER TABLE `ibadah`
+  ADD PRIMARY KEY (`kd_tempat`);
+
+--
+-- Indexes for table `kelurahan`
+--
+ALTER TABLE `kelurahan`
+  ADD PRIMARY KEY (`kd_kelurahan`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
